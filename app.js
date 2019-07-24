ad5c07a66c879b091d8920114d88763ae276e2ff
andreyromanov <andreygorogogo@gmail.com>
2019-07-24 22:38:54 +0300

    connected to mongoDB

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+require('dotenv/config');
+
+//Import routes
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
+
 //ROUTES
 
 app.get('/', (req,res) => {
-    res.send('We Are Home!!!');
+    res.send('We Are Home!!!Stopped at 10 min');
 });
 
-app.get('/posts', (req,res) => {
-    res.send('We Are Posts!!!');
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, ()=>{
+    console.log('connected mdb');
 });
 
 app.listen(8000);
